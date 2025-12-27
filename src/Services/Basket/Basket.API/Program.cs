d6d8d01f4c192bb7e15bec073c99da75af2af5f8
Yevhenii Mashnytskyi <yevhenii_mashnytskyi@epam.com>
2025-12-27 12:18:43 +0200

    add basket

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,50 @@
+using Basket.API.Data;
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var assembly = typeof(Program).Assembly;
+builder.Services.AddOpenApi();
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database"));
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database"))
+    .AddRedis(builder.Configuration.GetConnectionString("Redis"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.Run();
